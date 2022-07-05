b6ac6900a7e44dc3c6faa5e1bc97c8998101b7cd
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-07-06 00:33:08 +0200

    Run lint

--- blog/.eslintrc.js
+++ blog/.eslintrc.js
@@ -1,29 +1,29 @@
 const RULES = {
-  OFF: "off",
-  WARN: "warn",
-  ERROR: "error",
-};
+  OFF: 'off',
+  WARN: 'warn',
+  ERROR: 'error'
+}
 
 module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
+    node: true
   },
-  extends: ["plugin:vue/vue3-essential", "standard"],
+  extends: ['plugin:vue/vue3-essential', 'standard'],
   parserOptions: {
-    ecmaVersion: "latest",
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
   },
   plugins: [
-    "vue",
-    "@typescript-eslint",
-    "prettier", // Deactivate all conflict rules with ESLint
+    'vue',
+    '@typescript-eslint',
+    'prettier' // Deactivate all conflict rules with ESLint
   ],
   rules: {
-    "no-unused-vars": RULES.WARN,
-    "no-multiple-empty-lines": RULES.WARN,
-    "no-trailing-spaces": RULES.OFF,
-  },
-};
+    'no-unused-vars': RULES.WARN,
+    'no-multiple-empty-lines': RULES.WARN,
+    'no-trailing-spaces': RULES.OFF
+  }
+}
