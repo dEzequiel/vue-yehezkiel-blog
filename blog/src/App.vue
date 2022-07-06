601766fa6077425d87613a2fb6fcd896e4a034e7
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-07-06 22:26:00 +0200

    Try precommit

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">import HelloWorld from "./components/HelloWorld.vue"</script>
-
+a
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
