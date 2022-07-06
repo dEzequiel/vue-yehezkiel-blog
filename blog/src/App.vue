0e0d84309091a3560cfe2ee8c58d4e872806b597
Ezequiel De La Rosa <erosarosario@cifpfbmoll.eu>
2022-07-06 22:25:34 +0200

    Try precommit

--- blog/src/App.vue
+++ blog/src/App.vue
@@ -1,8 +1,4 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue"
-</script>
+<script setup lang="ts">import HelloWorld from "./components/HelloWorld.vue"</script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
